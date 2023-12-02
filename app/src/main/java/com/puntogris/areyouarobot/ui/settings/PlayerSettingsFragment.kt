30bddffab47a2c37bc8f470c0fa740177d941673
puntogris <rosariodota3@gmail.com>
2023-12-02 15:29:36 -0300

    updated website links

--- app/src/main/java/com/puntogris/areyouarobot/ui/settings/PlayerSettingsFragment.kt
+++ app/src/main/java/com/puntogris/areyouarobot/ui/settings/PlayerSettingsFragment.kt
@@ -1,6 +1,10 @@
 package com.puntogris.areyouarobot.ui.settings
 
+import android.content.Intent
+import android.net.Uri
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.puntogris.areyouarobot.R
 import com.puntogris.areyouarobot.SharedPref
 import com.puntogris.areyouarobot.databinding.FragmentPlayerSettingsBinding
@@ -21,6 +25,24 @@ class PlayerSettingsFragment : BaseFragment<FragmentPlayerSettingsBinding>(R.lay
             val input = binding.playerNameSettings.text.toString()
             sharedPref.setPlayerName(input)
             findNavController().navigate(R.id.welcomeFragment)
+        }
+
+        binding.privacyPolicy.setOnClickListener {
+            launchWebBrowserIntent("https://are-you-a-robot-web.vercel.app/privacy-policy.html")
+        }
+
+        binding.termsAndConditions.setOnClickListener {
+            launchWebBrowserIntent("https://are-you-a-robot-web.vercel.app/terms-and-conditions.html")
+        }
+    }
+
+    fun Fragment.launchWebBrowserIntent(uri: String) {
+        try {
+            Intent(Intent.ACTION_VIEW).let {
+                it.data = Uri.parse(uri)
+                startActivity(it)
+            }
+        } catch (e: Exception) {
         }
     }
 }
